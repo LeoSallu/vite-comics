429496c43575f9f9c433ac8460bf742cd6d902ca
LeoSallu <L.sallustio99@gmail.com>
2023-03-23 14:59:37 +0100

    v-for in AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,41 @@
 <script>
     export default{
-
+        data(){
+            return{
+            menuItem:[
+                {
+                    menuName: 'CHARACTERS'
+                },
+                {
+                    menuName: 'COMICS'
+                },
+                {
+                    menuName: 'MOVIES'
+                },
+                {
+                    menuName: 'TV'
+                },
+                {
+                    menuName: 'GAMES'
+                },
+                {
+                    menuName: 'COLLECTIBLES'
+                },
+                {
+                    menuName: 'VIDEOS'
+                },
+                {
+                    menuName: 'FANS'
+                },
+                {
+                    menuName: 'NEWS'
+                },
+                {
+                    menuName: 'SHOP'
+                }
+            ]
+        }
+        }
     }
 </script>
 
@@ -8,16 +43,31 @@
     <header>
         <div class="container">
             <div class="logo">
-                LOGO
+                <img src="public/img/dc-logo.png" alt="logo-dc">
             </div>  
             <div class="menu">
-                MENU
+                <ul class="menu-list">
+                    <li v-for="menu in menuItem">
+                        {{menu.menuName}}
+                    </li>
+                </ul>
             </div>
         </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
-
-
+    .container{
+        display: flex;
+        justify-content: space-between;
+        width: 700px;
+        margin: 0 auto;
+        .logo,.menu{
+            padding: 10px;
+        }
+        .menu .menu-list{
+            list-style: none;
+            display: flex;
+        }
+    }
 </style>
