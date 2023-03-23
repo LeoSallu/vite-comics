31a44eb97d49151ce1ecc518244067c441fe6246
LeoSallu <L.sallustio99@gmail.com>
2023-03-23 14:40:22 +0100

    First Commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+    export default{
+
+    }
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                LOGO
+            </div>  
+            <div class="menu">
+                MENU
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
